22444c520dcc66bd3494cf2c569c4bdf153124ae
JiYoung-Kwon <rhakrywkd@naver.com>
2021-05-31 17:19:34 +0900

    create board_search page

---
+++ WebContent/KJY/board/board.js
@@ -0,0 +1,12 @@
+/**
+ * 보드게임 소개 페이지 관리
+ */
+
+onDisplay = function(n){
+	var num = parseInt(n/4);
+	if($('.detailPage').eq(num).css('display') == 'none'){
+		$('.detailPage').eq(num).show();
+	}else{
+		$('.detailPage').eq(num).hide();
+	}
+}
